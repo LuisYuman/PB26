dccbb012748062e8259b5026a70144be59c45eb5
Luis Yuman <lyuman737@gmail.com>
2023-08-26 12:22:29 -0600

    Actualizado

--- src/main/java/edu/ejercicios/Main.java
+++ src/main/java/edu/ejercicios/Main.java
@@ -1,7 +1,48 @@
 package edu.ejercicios;
 
+import edu.ejercicios.pkgAlumnos.Alumno;
+import edu.ejercicios.pkgAlumnos.CursoAsignado;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<CursoAsignado>cat_cursos = new ArrayList<>();
+        cat_cursos.add(new CursoAsignado(101, "Programacion",500));
+        cat_cursos.add(new CursoAsignado(105, "Derecho",1));
+        cat_cursos.add(new CursoAsignado(110, "Estadistica",450));
+        cat_cursos.add(new CursoAsignado(120, "Electronica",400));
+
+
+        Alumno al1 = new Alumno("2023015", "Andrea", "55555", "5 calle 8-36 zona 1");
+        al1.agregarCursoAsignado(cat_cursos.get(1));
+        al1.agregarCursoAsignado(cat_cursos.get(2));
+        al1.mostrarInformacion();
+        System.out.println("Monto a pagar: "+ al1.calcularMonto());
+
+        Alumno al2 = new Alumno("2020055", "Monica", "1111", "8-36 zona 10");
+        al2.agregarCursoAsignado(cat_cursos.get(0));
+        al2.agregarCursoAsignado(cat_cursos.get(3));
+        al2.agregarCursoAsignado(cat_cursos.get(1));
+        al2.mostrarInformacion();
+        System.out.println("Monto a pagar: "+ al2.calcularMonto());
+
+        Alumno al3 = new Alumno("20226550", "Luis", "4444", "8-36 zona 12");
+        al3.agregarCursoAsignado(cat_cursos.get(0));
+        al3.agregarCursoAsignado(cat_cursos.get(1));
+        al3.agregarCursoAsignado(cat_cursos.get(2));
+        al3.mostrarInformacion();
+        System.out.println("Monto a pagar: "+ al3.calcularMonto());
+
+        Alumno al4 = new Alumno("2020554", "David", "33333", "8-36 zona 12");
+        al4.agregarCursoAsignado(cat_cursos.get(0));
+        al4.agregarCursoAsignado(cat_cursos.get(2));
+        al4.agregarCursoAsignado(cat_cursos.get(3));
+        al4.mostrarInformacion();
+        System.out.println("Monto a pagar: "+ al4.calcularMonto());
+
+        System.out.println((al1.calcularMonto()+al2.calcularMonto()+al3.calcularMonto()+al4.calcularMonto()));
+
     }
 }
--- src/main/java/edu/ejercicios/pkgAlumnos/Alumno.java
+++ src/main/java/edu/ejercicios/pkgAlumnos/Alumno.java
@@ -1,2 +1,88 @@
-package edu.ejercicios.pkgAlumnos;public class Alumno {
+package edu.ejercicios.pkgAlumnos;
+
+import java.util.ArrayList;
+
+public class Alumno {
+    private String carnet;
+    public String nombre;
+    public String telefono;
+    public String direccion;
+    private ArrayList<CursoAsignado> cursoAsignados;
+
+    public Alumno(String carnet, String nombre, String telefono, String direccion) {
+        this.carnet = carnet;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.cursoAsignados = new ArrayList<>();
+    }
+
+    public void agregarCursoAsignado(CursoAsignado curso){
+        cursoAsignados.add(curso);
+    }
+
+    public double calcularMonto(){
+        double monto = 0;
+        for (CursoAsignado curso: cursoAsignados){
+            monto += curso.getCostoCurso();
+        }
+        return monto;
+    }
+
+    @Override
+    public String toString(){
+        return "Carnet: " + this.carnet + " Nombre: " + this.nombre + " Direccion: " + this.direccion;
+    }
+
+    public void mostrarInformacion(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Cursos Asignados: ");
+        for (CursoAsignado curso: cursoAsignados){
+            System.out.println("Curso: " + curso.getNombreCurso());
+            System.out.println("Precio: " + curso.getCostoCurso());
+            System.out.println("--------------------");
+        }
+    }
+
+    //Getters and Setters
+
+    public String getCarnet() {
+        return carnet;
+    }
+
+    public void setCarnet(String carnet) {
+        this.carnet = carnet;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public ArrayList<CursoAsignado> getCursoAsignados() {
+        return cursoAsignados;
+    }
+
+    public void setCursoAsignados(ArrayList<CursoAsignado> cursoAsignados) {
+        this.cursoAsignados = cursoAsignados;
+    }
 }
